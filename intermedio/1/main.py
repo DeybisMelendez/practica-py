9d296b994d6c5f6b4a317fbe43d560741de98ca0
DeybisMelendez <pixelweb2007@gmail.com>
2023-03-12 11:57:47 -0600

    añadido ultimo punto del ejercicio 1

--- intermedio/1/main.py
+++ intermedio/1/main.py
@@ -2,6 +2,13 @@ from random_array import get_random_array
 from sort import selection_sort, bubble_sort, quick_sort
 
 
+def average(array):
+    sum = 0
+    for i in array:
+        sum += i
+    return i/len(array)
+
+
 def get_ten_first(array):
     new_array = []
     for x in range(10):
@@ -51,3 +58,8 @@ if __name__ == "__main__":
     print("Array ordenado")
     print("Los primeros diez son:", get_ten_first(copy))
     print("Los ultimos diez son:", get_ten_last(copy))
+
+    print("--- Calculando el maximo, minimo y promedio ---")
+    print("El maximo es:", copy[len(copy)-1])
+    print("El minimo es:", copy[0])
+    print("El promedio es:", average(copy))
